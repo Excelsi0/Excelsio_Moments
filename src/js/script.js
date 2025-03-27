730bd31938c4c173adb879431d9293801508d02a
Excelsio <vapeexcelsio@gmail.com>
2025-03-27 22:08:53 +0300

    попытка убрать hover/active в мобильной версии

--- src/js/script.js
+++ src/js/script.js
@@ -3,3 +3,7 @@ document.addEventListener("mousemove", function(dets){
     cursor.style.left = dets.pageX+"px"
     cursor.style.top = dets.pageY+"px"
 })
+if ('ontouchstart' in window || navigator.maxTouchPoints) {
+  document.querySelector('.cursor').style.display = 'none';
+}
+
